481d51cd9fa6c02ead5fa5a040c6ac567dd6511f
JoaquinFritz16 <162236965+JoaquinFritz16@users.noreply.github.com>
2024-05-22 21:34:38 -0300

    Update ej3.py

--- ej3.py
+++ ej3.py
@@ -1,26 +1,25 @@
 import requests
 import pyperclip
 from bs4 import BeautifulSoup
-from selenium import webdriver
-
-def save_html(response):
+def save_html():
     soup = BeautifulSoup(response.text, 'html.parser')
     articulos = soup.find_all('a')
     for a in articulos:
         enlace = a.get("href")
-        if enlace and 'pagination' in enlace:
+        if enlace.lower().startswith("https://chequeado.com/blog/?current-page"):
             lista.append(enlace)
     print(lista)
 
-lista = []    
-
+lista=[]    
 while True:
     try:
         url = pyperclip.paste()
+        print(url)
         response = requests.get(url)
+        print(response)
         response.raise_for_status()
-        save_html(response)
+        save_html()
         break
     except requests.exceptions.RequestException as e:
-        print('Por favor, elige una URL válida: ', e)
+        print('Flaco elegi una url de una pagina: ', e)
         break
